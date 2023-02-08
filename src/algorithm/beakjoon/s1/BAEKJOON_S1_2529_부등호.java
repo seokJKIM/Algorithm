6e1e913557343838d3afc3324ad631ecf24cd1ba
seokJKIM <tjrwn1247@naver.com>
2023-02-09 02:10:16 +0900

    0209

--- src/algorithm/beakjoon/s1/BAEKJOON_S1_2529_부등호.java
+++ src/algorithm/beakjoon/s1/BAEKJOON_S1_2529_부등호.java
@@ -3,6 +3,8 @@ package algorithm.beakjoon.s1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BAEKJOON_S1_2529_부등호 {
@@ -10,16 +12,17 @@ public class BAEKJOON_S1_2529_부등호 {
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
 	static char[] arr;
-	static int max, min;
-	static String maxAns, minAns;
-	static int[] choosed;
-	public static void main(String[] args) throws IOException {
-	}
 	
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(input.readLine());
+	
+		
+		
+	}
 		
 	
 	public static boolean check(int num1, int num2, int arrIdx) {
-		if(arr[arrIdx] == '<') {
+		if(arr[arrIdx-1] == '<') {
 			return num1 < num2;
 		}else {
 			return num1 > num2;
