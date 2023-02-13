44f37d7b6008ca42301daea480c952291d68bd2f
seokJKIM <tjrwn1247@naver.com>
2023-02-13 23:34:37 +0900

    0213

--- src/algorithm/beakjoon/s1/BAEKJOON_S1_2468_안전영역.java
+++ src/algorithm/beakjoon/s1/BAEKJOON_S1_2468_안전영역.java
@@ -5,6 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * @author seok
+ * @since 2023.02.13
+ * @see https://www.acmicpc.net/problem/2468
+ * @performance 15772 kb	200 ms
+ * @category #dfs
+ * @note
+ */
+
 public class BAEKJOON_S1_2468_안전영역 {
 	
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -12,42 +21,46 @@ public class BAEKJOON_S1_2468_안전영역 {
 	static StringTokenizer tokens;
 	static int[][] arr;
 	static boolean[][] check;
-	static int max;
-	static int N;
-	
+	static int max, N, ans;
+	static int[][] deltas = {{-1,0},{0,1},{1,0},{0,-1}};
 	public static void main(String[] args) throws IOException {
 		N = Integer.parseInt(input.readLine());
 		arr = new int[N][N];
-		check = new boolean[N][N];
 		max = 0;
 		for(int i=0; i<N; i++) {
 			tokens = new StringTokenizer(input.readLine());
 			for(int j=0; j<N; j++) {
 				arr[i][j] = Integer.parseInt(tokens.nextToken());
+				max = Math.max(max, arr[i][j]);
 			}
 		}
-		
-		for(int a=1; a<=100; a++) {
-			for(int i=0; i<arr.length; i++) {
-				for(int j=0; j<arr[j].length; j++) {
-					if(arr[i][j] == a) {
-						check[i][j] = true;
+		ans = 0;
+		for(int i=0; i<=max; i++) {
+			check = new boolean[N][N];
+			int cnt = 0;
+			for(int j=0; j<N; j++) {
+				for(int k=0; k<N; k++) {
+					if(!check[j][k] && arr[j][k] > i) {
+						dfs(j,k,i);
+						cnt++;
 					}
 				}
 			}
-			
-			for(int i=0; i<arr.length; i++) {
-				for(int j=0; j<arr[i].length; j++) {
-					dfs(i,j);
-				}
-			}
+			ans = Math.max(ans, cnt);
 		}
 		
+		System.out.println(ans);
 	}
 	
-	static void dfs(int x, int y) {
-		if(!check[x][y]) {
+	static void dfs(int x, int y, int height) {
+		check[x][y] = true;
+		for(int i=0; i<4; i++) {
+			int nx = x+deltas[i][0];
+			int ny = y+deltas[i][1];
 			
+			if(isIn(nx,ny) && !check[nx][ny] && arr[nx][ny] > height) {
+				dfs(nx,ny,height);
+			}
 		}
 	}
 	
