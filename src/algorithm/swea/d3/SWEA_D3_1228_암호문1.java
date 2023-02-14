64a3bcb98d2b4b255ba098c954c112d0bb6160a1
seokJKIM <tjrwn1247@naver.com>
2023-02-14 17:48:59 +0900

    0214

--- src/algorithm/swea/d3/SWEA_D3_1228_암호문1.java
+++ src/algorithm/swea/d3/SWEA_D3_1228_암호문1.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-// 
-
 
 /**
  * @author seok
--- src/ex.java
+++ src/ex.java
@@ -10,53 +10,22 @@ public class ex{
 	static StringTokenizer tokens;
 	static char[] arr= {'a','b','c','d','e'};
 	public static void main(String[] args) {
-//		makePermutation(0,new char[3], new boolean[arr.length]);
+		makePermutation(0,new char[3], new boolean[arr.length]);
 		
 //		makeCombination(0, new char[3], 0);
 		
-		powerSet(0, new boolean[arr.length]);
+//		powerSet(0, new boolean[arr.length]);
 	}
 	
-	static void makePermutation(int lv, char[] choosed, boolean[] check ) {
-		if(lv == choosed.length) {
+	static void makePermutation(int lv, char[] choosed, boolean[] check) {
+		if(lv==choosed.length) {
 			System.out.println(Arrays.toString(choosed));
-			return;
 		}
 		
 		for(int i=0; i<arr.length; i++) {
 			if(!check[i]) {
-				choosed[lv] = arr[i];
-				check[i] = true;
-				makePermutation(lv+1, choosed, check);
-				check[i] = false;
+				
 			}
 		}
-	}
-	
-	static void makeCombination(int lv, char[] choosed, int startIdx) {
-		if(lv == choosed.length) {
-			System.out.println(Arrays.toString(choosed));
-			return;
-		}
-		
-		for(int i=startIdx; i<arr.length; i++) {
-			choosed[lv] = arr[i];
-			makeCombination(lv+1,choosed,i+1);
-		}
-	}
-	
-	static void powerSet(int lv, boolean[] check) {
-		if(lv == check.length) {
-			for(int i=0; i<check.length; i++) {
-				if(check[i]) System.out.print(arr[i]+" ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		check[lv] = true;
-		powerSet(lv+1, check);
-		check[lv] = false;
-		powerSet(lv+1, check);
 	}
 }
