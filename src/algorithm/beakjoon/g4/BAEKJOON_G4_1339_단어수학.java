ac56596f84440cf55336eb1c15eb41ffbf8016a1
seokJKIM <tjrwn1247@naver.com>
2023-02-20 21:44:18 +0900

    0220

---
+++ src/algorithm/beakjoon/g4/BAEKJOON_G4_1339_단어수학.java
@@ -0,0 +1,20 @@
+package algorithm.beakjoon.g4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON_G4_1339_단어수학 {
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(input.readLine());
+		
+		for(int i=0; i<N; i++) {
+		}
+	}
+}
