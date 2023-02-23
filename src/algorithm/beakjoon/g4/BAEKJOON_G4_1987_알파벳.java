73129700ddc7e674015576b5dfad67763a9a8a7b
seokJKIM <tjrwn1247@naver.com>
2023-02-23 22:08:06 +0900

    0223

--- src/algorithm/beakjoon/g4/BAEKJOON_G4_1987_알파벳.java
+++ src/algorithm/beakjoon/g4/BAEKJOON_G4_1987_알파벳.java
@@ -5,7 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BAEKJOON_g4_1987_알파벳 {
+/**
+ * @author seok
+ * @since 2023.02.23
+ * @see https://www.acmicpc.net/problem/1987
+ * @performance 12620 kb	1312 ms
+ * @category # dfs
+ * @note
+ */
+
+public class BAEKJOON_G4_1987_알파벳 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
@@ -28,28 +37,24 @@ public class BAEKJOON_g4_1987_알파벳 {
 				arr[i][j] = st.charAt(j);
 			}
 		}
-		check[arr[0][0]-'A'] = true;
-		count(0, 0, 1);
+		count(0, 0, 0);
 
 		System.out.println(ans);
 	}
 
 	public static void count(int x, int y, int lv) {
-		
+		if (check[arr[x][y] - 'A']) {
+			ans = Math.max(ans, lv);
+			return;
+		}
 		for (int i = 0; i < 4; i++) {
 			int nx = x + deltas[i][0];
 			int ny = y + deltas[i][1];
 
 			if (isIn(nx, ny)) {
-				if (check[arr[nx][ny] - 'A']) {
-					ans = Math.max(ans, lv);
-					return;
-				}
-				else {
-					check[arr[nx][ny] - 'A'] = true;
-					count(nx, ny, lv + 1);
-					check[arr[nx][ny] - 'A'] = false;
-				}
+				check[arr[x][y] - 'A'] = true;
+				count(nx, ny, lv + 1);
+				check[arr[x][y] - 'A'] = false;
 			}
 		}
 	}
