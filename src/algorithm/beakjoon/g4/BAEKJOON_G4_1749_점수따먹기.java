d1df85f0a48a6f4af661f8fcca94e819d5ce5ddb
seokJKIM <tjrwn1247@naver.com>
2023-02-23 09:52:35 +0900

    0223

--- src/algorithm/beakjoon/g4/BAEKJOON_G4_1749_점수따먹기.java
+++ src/algorithm/beakjoon/g4/BAEKJOON_G4_1749_점수따먹기.java
@@ -5,6 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * @author seok
+ * @since 2023.02.23
+ * @see https://www.acmicpc.net/problem/1749
+ * @performance 19044 kb	856 ms
+ * @category # 누적합
+ * @note
+ */
+
 public class BAEKJOON_G4_1749_점수따먹기 {
 	
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -12,6 +21,7 @@ public class BAEKJOON_G4_1749_점수따먹기 {
 	static StringTokenizer tokens;
 	static int[][] arr;
 	static int N,M;
+	static int max;
 	public static void main(String[] args) throws IOException {
 		tokens = new StringTokenizer(input.readLine());
 		N = Integer.parseInt(tokens.nextToken());
@@ -29,16 +39,21 @@ public class BAEKJOON_G4_1749_점수따먹기 {
 				arr[i][j] = arr[i][j] + arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1];
 			}
 		}
-
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=M; j++) {
-				System.out.print(arr[i][j]+" ");
+		
+		max = Integer.MIN_VALUE;
+		int sum = 0;
+		
+		for(int x1=1; x1<=N; x1++) {
+			for(int y1=1; y1<=N; y1++) {
+				for(int x2=x1; x2<=N; x2++) {
+					for(int y2=y1; y2<=N; y2++) {
+						sum = arr[x2][y2]-arr[x2][y1-1]-arr[x1-1][y2]+arr[x1-1][y1-1];
+						max = Math.max(sum, max);
+					}
+				}
 			}
-			System.out.println();
 		}
-
 		
-		
-		
+		System.out.println(max);
 	}
 }
