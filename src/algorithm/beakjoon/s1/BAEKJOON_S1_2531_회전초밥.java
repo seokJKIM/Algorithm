e090036fe23baf57b4f5b1f97e213ed73c4790ed
seokJKIM <tjrwn1247@naver.com>
2023-02-23 12:22:14 +0900

    0223

--- src/algorithm/beakjoon/s1/BAEKJOON_S1_2531_회전초밥.java
+++ src/algorithm/beakjoon/s1/BAEKJOON_S1_2531_회전초밥.java
@@ -1,6 +1,7 @@
 package algorithm.beakjoon.s1;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
@@ -9,8 +10,19 @@ public class BAEKJOON_S1_2531_회전초밥 {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
+	static int N,D,K,C;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(input.readLine());
+		
+		N = Integer.parseInt(tokens.nextToken());
+		D = Integer.parseInt(tokens.nextToken());
+		K = Integer.parseInt(tokens.nextToken());
+		C = Integer.parseInt(tokens.nextToken());
+		
+		for(int i=0; i<N; i++) {
+			
+		}
 		
 	}
 }
