93cd3fee09227c43d5d993b72381d56b821b097d
seokJKIM <tjrwn1247@naver.com>
2023-02-24 14:18:52 +0900

    0224

--- src/algorithm/beakjoon/s4/BAEKJOON_S4_10157_자리배정.java
+++ src/algorithm/beakjoon/s4/BAEKJOON_S4_10157_자리배정.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
+/**
  * @author seok
  * @since 2023.02.24
  * @see https://www.acmicpc.net/problem/10157
- * @performance 12168 kb	96 ms
- * @category # 백트래킹
+ * @performance 19684 kb	128 ms
+ * @category # 구현
  * @note
  */
 
@@ -34,48 +34,59 @@ public class BAEKJOON_S4_10157_자리배정 {
 		int nowx = R-1;
 		int nowy = 0;
 		int cnt = 1;
-		while(true) {
-			
-			for(int i=0; i<R-n; i++) {
-				map[nowx--][nowy] = cnt++;
+		if(N <= R*C) {
+			while(true) {
+				
+				for(int i=0; i<R-n; i++) {
+					map[nowx--][nowy] = cnt++;
+				}
+				
+				nowx++;
+				nowy++;
+				n++;
+				
+				if(cnt > N) break;
+				
+				for(int i=0; i<C-n; i++) {
+					map[nowx][nowy++] = cnt++;
+					
+				}
+				
+				nowy--;
+				nowx++;
+				
+				if(cnt > N) break;
+				
+				for(int i=0; i<R-n; i++) {
+					map[nowx++][nowy] = cnt++;
+				}
+				
+				nowx--;
+				nowy--;
+				n++;
+				
+				if(cnt > N) break;
+				
+				for(int i=0; i<C-n; i++) {
+					map[nowx][nowy--] = cnt++;
+				}
+				
+				nowy++;
+				nowx--;
+				
+				if(cnt > N) break;
 			}
-			
-			nowx++;
-			nowy++;
-			n++;
-			
-			for(int i=0; i<C-n-1; i++) {
-				map[nowx][nowy++] = cnt++;
-			}
-			
-			for(int i=0; i<R-n+1; i++) {
-				map[nowx++][nowy] = cnt++;
-			}
-			
-			nowx--;
-			n++;
 			
 			for(int i=0; i<R; i++) {
-				map[nowx][nowy--] = cnt++;
+				for(int j=0; j<C; j++) {
+					if(map[i][j] == N) {
+						System.out.println((j+1)+" "+(R-i));
+						System.exit(0);
+					}
+				}
 			}
-			
-			nowy++;
-			
-			if(cnt >= N) break;
+		}else {
+			System.out.println(0);
 		}
-		
-		for(int i=0; i<R; i++) {
-			for(int j=0; j<C; j++) {
-				System.out.print(map[i][j]+" ");
-//				if(map[i][j] == N) {
-//					System.out.println((R-i)+" "+(C-j));
-//					System.exit(0);
-//				}
-			}
-			System.out.println();
-		}
-		System.out.println(0);
-		
-		
 	}
 }
