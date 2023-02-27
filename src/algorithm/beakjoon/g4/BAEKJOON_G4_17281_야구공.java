74bc245d9a22261a5e136ceabc37efa7c51a660d
seokJKIM <tjrwn1247@naver.com>
2023-02-27 11:19:12 +0900

    0227

---
+++ src/algorithm/beakjoon/g4/BAEKJOON_G4_17281_야구공.java
@@ -0,0 +1,28 @@
+package algorithm.beakjoon.g4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author seok
+ * @since 
+ * @see 
+ * @performance 
+ * @category 
+ * @note
+ */
+public class BAEKJOON_G4_17281_야구공 {
+	
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	static int N;
+	
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(input.readLine());
+		
+		
+	}
+}
