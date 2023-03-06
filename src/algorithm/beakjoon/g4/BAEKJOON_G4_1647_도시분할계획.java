2498731843adbaf594d4cdde1cb266fbb47e04c1
seokJKIM <tjrwn1247@naver.com>
2023-03-06 11:27:58 +0900

    0306

---
+++ src/algorithm/beakjoon/g4/BAEKJOON_G4_1647_도시분할계획.java
@@ -0,0 +1,94 @@
+package algorithm.beakjoon.g4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BAEKJOON_G4_1647_도시분할계획 {
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	static int N,M;
+	static long sum;
+	static int[] repres;
+	static Edge[] EdgeArr;
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(input.readLine());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
+		sum = 0;
+		
+		repres = new int[N+1];
+		EdgeArr = new Edge[M];
+		
+		makeSet();
+		
+		for(int test_case=0; test_case<M; test_case++) {
+			tokens = new StringTokenizer(input.readLine());
+			int a = Integer.parseInt(tokens.nextToken());
+			int b = Integer.parseInt(tokens.nextToken());
+			int c = Integer.parseInt(tokens.nextToken());
+		
+			EdgeArr[test_case] = new Edge(a,b,c);
+		}
+		
+		Arrays.sort(EdgeArr);
+		
+		for(int i=0; i<EdgeArr.length; i++) {
+			Edge e = EdgeArr[i];
+			
+			if(union(e.a, e.b)) {
+				sum += e.c;
+			}
+		}
+		System.out.println(Arrays.toString(repres));
+		
+		System.out.println(sum);
+	}
+	
+	public static void makeSet() {
+		for(int i=1; i<=N; i++) {
+			repres[i] = i;
+		}
+	}
+	
+	public static int findSet(int a) {
+		if(repres[a] == a) {
+			return a;
+		}else {
+			return repres[a] = findSet(repres[a]);
+		}
+	}
+	
+	public static boolean union(int a, int b) {
+		a = findSet(a);
+		b = findSet(b);
+		
+		if(a==b) {
+			return false;
+		}else {
+			repres[a] = b;
+			return true;
+		}
+	}
+	
+	public static class Edge implements Comparable<Edge>{
+		int a;
+		int b;
+		int c;
+		
+		public Edge(int a, int b, int c) {
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.c, o.c);
+		}
+	}
+}
