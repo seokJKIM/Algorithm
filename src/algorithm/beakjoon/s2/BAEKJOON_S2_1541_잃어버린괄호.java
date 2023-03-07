b2a57e3f0c551ae3ff014bcaeae0960d9d5b3fcc
seokJKIM <tjrwn1247@naver.com>
2023-03-07 20:30:23 +0900

    23.03.07

---
+++ src/algorithm/beakjoon/s2/BAEKJOON_S2_1541_잃어버린괄호.java
@@ -0,0 +1,33 @@
+package algorithm.beakjoon.s2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON_S2_1541_잃어버린괄호 {
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens;
+	static int sum;
+	public static void main(String[] args) throws IOException {
+		tokens = new StringTokenizer(input.readLine(),"-");
+		sum = Integer.MAX_VALUE;
+		while(tokens.hasMoreTokens()) {
+			int tmp = 0;
+			
+			StringTokenizer tokens2 = new StringTokenizer(tokens.nextToken(),"+");
+			
+			while(tokens2.hasMoreTokens()) {
+				tmp += Integer.parseInt(tokens2.nextToken());
+			}
+			
+			if(sum == Integer.MAX_VALUE) {
+				sum = tmp;
+			}else {
+				sum -= tmp;
+			}
+		}
+		System.out.println(sum);
+	}
+}
