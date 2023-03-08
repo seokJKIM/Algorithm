c018a652c008c020aefbc8683a584c5161667f08
seokJKIM <tjrwn1247@naver.com>
2023-03-08 10:10:05 +0900

    23.03.08

--- src/algorithm/beakjoon/s4/BAEKJOON_S4_2003_수들의합2.java
+++ src/algorithm/beakjoon/s4/BAEKJOON_S4_2003_수들의합2.java
@@ -3,7 +3,17 @@ package algorithm.beakjoon.s4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+
+/*
+ * @author seok
+ * @since 2023.03.08
+ * @see https://www.acmicpc.net/problem/2003
+ * @performance 14364 kb	116 ms
+ * @category # 누적합+투포인터
+ * @note
+ */
 
 public class BAEKJOON_S4_2003_수들의합2 {
 	
@@ -11,17 +21,39 @@ public class BAEKJOON_S4_2003_수들의합2 {
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
 	static int[] arr;
-	
+	static int N,cnt;
+	static long M;
 	public static void main(String[] args) throws IOException {
 		tokens = new StringTokenizer(input.readLine());
 		
-		int N = Integer.parseInt(tokens.nextToken());
-		int M = Integer.parseInt(tokens.nextToken());
+		N = Integer.parseInt(tokens.nextToken());
+		M = Integer.parseInt(tokens.nextToken());
 		
 		tokens = new StringTokenizer(input.readLine());
 		
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(tokens.nextToken());
+		arr = new int[N+1];
+		for(int i=1; i<=N; i++) {
+			arr[i] = arr[i-1]+Integer.parseInt(tokens.nextToken());
 		}
+		int cnt = 0;
+
+		int left = 0;
+		int right = 1;
+		
+		while(true) {
+			int sum = arr[right]-arr[left];
+			
+			if(sum == M) cnt++;
+			
+			if(sum > M) {
+				left++;
+			}else {
+				right++;
+			}
+			
+			if(right > N) break;
+		}
+		
+		System.out.println(cnt);
 	}
 }
