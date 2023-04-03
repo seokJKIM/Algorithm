9a2a91eec547afc0a147d614e22a01b43d5bc3f0
seokJKIM <tjrwn1247@naver.com>
2023-04-03 20:07:28 +0900

    23.04.03

--- src/algorithm/baekjoon/g4/BAEKJOON_G4_3055_탈출.java
+++ src/algorithm/baekjoon/g4/BAEKJOON_G4_3055_탈출.java
@@ -9,17 +9,25 @@ import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+* @author seok
+* @since 2023.04.03
+* @see https://www.acmicpc.net/problem/3055
+* @performance 	11848 kb	84 ms
+* @category # bfs
+* @note
+*/
+
 public class BAEKJOON_G4_3055_탈출 {
 
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
-	static int R, C;
+	static int R, C, ans;
 	static int[][] deltas = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
 	static char[][] map;
-	static int[][] intMap;
-	static boolean [][] visited;
-static Point end;
+	static Queue<Point> q = new LinkedList<>();
+	static Queue<Water> water = new LinkedList<>();
 	public static void main(String[] args) throws IOException {
 		tokens = new StringTokenizer(input.readLine());
 
@@ -29,123 +37,98 @@ static Point end;
 		// 물 = *, 돌 = X, 굴 = D, 고슴도치 = S
 
 		map = new char[R][C];
-		intMap = new int[R][C];
-		visited = new boolean[R][C];
-		end = null;
-		Point start = null;
 		for (int r = 0; r < R; r++) {
 			String tmp = input.readLine();
 			for (int c = 0; c < C; c++) {
 				map[r][c] = tmp.charAt(c);
-				if (map[r][c] == 'D') {
-					intMap[r][c] = Integer.MAX_VALUE;
-					end = new Point(r, c);
-				}
-					
+				
 				if (map[r][c] == 'S') {
-					start = new Point(r, c);
-					map[r][c] = '.';
+					q.add(new Point(r, c, 0));
+				}else if (map[r][c] == '*') {
+					water.add(new Water(r,c));
 				}
-				if (map[r][c] == '*') {
-					visited[r][c] = true;
-				}
-					
 			}
 		}
 
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				if (map[r][c] == '*') {
-					bfs(r, c);
-				}
-			}
-		}
+		ans = Integer.MAX_VALUE;
 		
-		move(start.x, start.y);
+		bfs();
+		
+		if(ans == Integer.MAX_VALUE) {
+			System.out.println("KAKTUS");
+		}else {
+			System.out.println(ans);
+		}
 	}
 
-	public static void move(int r, int c) {
-		Queue<Point> q = new LinkedList<>();
-		visited[r][c] = true;
-		q.add(new Point(r, c));
-
-		int cnt = 1;
-		boolean flag = false;
-		
-		outer : while (!q.isEmpty()) {
-			int size = q.size();
-			for (int s = 0; s < size; s++) {
-				Point p = q.poll();
-				if(p.x == end.x && p.y == end.y) {
-					flag = true;
-					break outer;
-				}
-				for (int i = 0; i < 4; i++) {
-					int nr = p.x + deltas[i][0];
-					int nc = p.y + deltas[i][1];
+	public static void bfs() {
+		// 한번 while 돌 때 물채우고 이동
+		// 물 차면 map *로 갱신
+		// .만나면 그 자리를 출발지점으로 선택하고 lv+1
+		// 물채우기
+		while(!q.isEmpty()) {
+			int size = water.size();
+			
+			for(int i=0; i<size; i++) {
+				Water w = water.poll();
 				
-					if(isIn(nr,nc) && map[nr][nc] != 'X' && !visited[nr][nc] && cnt < intMap[nr][nc]) {
-						
-						visited[nr][nc] = true;
-						q.add(new Point(nr,nc));
+				for(int j=0; j<4; j++) {
+					int nr = w.x + deltas[j][0];
+					int nc = w.y + deltas[j][1];
+					
+					if(isIn(nr,nc) && map[nr][nc] == '.') {
+						map[nr][nc] = '*';
+						water.add(new Water(nr,nc));
 					}
 				}
 			}
 			
-			cnt++;
-		}
-		
-		
-		if(flag) {
-			System.out.println(cnt-1);
-		}else {
-			System.out.println("KAKTUS");
-		}
-		
-	}
-
-	public static void bfs(int r, int c) {
-		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(r, c));
-		boolean visit[][] = new boolean[R][C];
-		visit[r][c] = true;
-		int cnt = 1;
-		while (!q.isEmpty()) {
-			int size = q.size();
-			for (int s = 0; s < size; s++) {
+			size = q.size();
+			
+			for(int i=0; i<size; i++) {
 				Point p = q.poll();
-
-				for (int i = 0; i < 4; i++) {
-					int nr = p.x + deltas[i][0];
-					int nc = p.y + deltas[i][1];
-					if (isIn(nr, nc) && map[nr][nc] == '.' && !visit[nr][nc]) {
-						visit[nr][nc] = true;
-						if (intMap[nr][nc] != 0) {
-							if (intMap[nr][nc] > cnt) {
-								intMap[nr][nc] = cnt;
-							}
-						} else {
-							intMap[nr][nc] = cnt;
+				
+				for(int j=0; j<4; j++) {
+					int nr = p.x+deltas[j][0];
+					int nc = p.y+deltas[j][1];
+					
+					if(isIn(nr, nc)) {
+						if(map[nr][nc] == 'D') {
+							ans = Math.min(ans, p.lv+1);
+							return;
+						}else if(map[nr][nc] == '.') {
+							map[nr][nc] = 'S';
+							q.add(new Point(nr,nc,p.lv+1));
 						}
-						q.add(new Point(nr, nc));
 					}
 				}
 			}
-			cnt++;
 		}
 	}
+	
 
 	public static boolean isIn(int r, int c) {
 		return 0 <= r && r < R && 0 <= c && c < C;
 	}
 
+	static class Water{
+		int x;
+		int y;
+		public Water(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
 	static class Point {
 		int x;
 		int y;
-
-		public Point(int x, int y) {
+		int lv;
+		
+		public Point(int x, int y, int lv) {
 			this.x = x;
 			this.y = y;
+			this.lv = lv;
 		}
 	}
 }
