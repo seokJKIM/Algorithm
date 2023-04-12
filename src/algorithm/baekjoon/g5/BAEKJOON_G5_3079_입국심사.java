3922626b85511da41ac62d822b7c8358080426b6
seokJKIM <tjrwn1247@naver.com>
2023-04-12 22:53:46 +0900

    23.04.12

--- src/algorithm/baekjoon/g5/BAEKJOON_G5_3079_입국심사.java
+++ src/algorithm/baekjoon/g5/BAEKJOON_G5_3079_입국심사.java
@@ -3,34 +3,43 @@ package algorithm.baekjoon.g5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+
+/*
+* @author seok
+* @since 2023.04.12
+* @see https://www.acmicpc.net/problem/3079
+* @performance 22824 kb	280 ms
+* @category # 이분탐색
+* @note
+*/
+
 
 public class BAEKJOON_G5_3079_입국심사 {
 
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
-	static int N, M;
+	static long N, M, ans;
 	static long[] arr;
 	public static void main(String[] args) throws IOException {
 		tokens = new StringTokenizer(input.readLine());
 		// N 입국 심사대 수
-		N = Integer.parseInt(tokens.nextToken());
+		N = Long.parseLong(tokens.nextToken());
 		// M 친구 수
-		M = Integer.parseInt(tokens.nextToken());
+		M = Long.parseLong(tokens.nextToken());
 	
 		
-		arr = new long[N];
-		long max = Long.MIN_VALUE;
-		
+		arr = new long[(int)N];
+		long max = 0;
+		ans = Long.MAX_VALUE;
 		for(int t=0; t<N; t++) {
 			arr[t] = Integer.parseInt(input.readLine());
 			max = Math.max(max, arr[t]);
 		}
 		
 		long right = max*M;
-		long left = 0;
+		long left = 1;
 
 		while(left <= right) {
 			long mid = (right+left)/2;
@@ -38,12 +47,15 @@ public class BAEKJOON_G5_3079_입국심사 {
 			
 			for(int i=0; i<arr.length; i++) {
 				cnt += mid/arr[i];
+				
+				if(cnt >= M) break;
 			}
 			
 			if(cnt < M) {
 				left = mid+1;
 			}else {
 				right = mid-1;
+				ans = Math.min(ans, mid);
 			}
 				
 		}
