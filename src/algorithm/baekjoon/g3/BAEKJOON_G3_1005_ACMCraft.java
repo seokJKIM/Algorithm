233f6e637f3f6f7306b2c8b156117f92bf8cba63
seokJKIM <tjrwn1247@naver.com>
2023-04-19 23:44:38 +0900

    23.04.19

---
+++ src/algorithm/baekjoon/g3/BAEKJOON_G3_1005_ACMCraft.java
@@ -0,0 +1,18 @@
+package algorithm.baekjoon.g3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON_G3_1005_ACMCraft {
+	
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	public static void main(String[] args) throws IOException {
+		int T = Integer.parseInt(input.readLine());
+		
+	}
+}
